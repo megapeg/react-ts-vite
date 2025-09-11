43596aa7ef0c8744e6218b28c1497208b36a09f8
Alex Pegorini <alex_pegorini@hotmail.com>
2025-09-11 21:57:42 +0100

    add support for environment vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const appName = import.meta.env.VITE_APP_NAME;
+  const mode = import.meta.env.MODE;
 
   return (
     <>
@@ -16,7 +18,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        {appName} ({mode})
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
