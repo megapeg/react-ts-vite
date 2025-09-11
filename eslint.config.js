9851d165b3906a054fb870711ce52779bfccec89
Alex Pegorini <alex_pegorini@hotmail.com>
2025-09-11 15:33:19 +0100

    Fix linting issue

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
