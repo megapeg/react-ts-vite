702e2d1b2e13426c633aff4db6817e4ff94e17f8
Alex Pegorini <alex_pegorini@hotmail.com>
2025-10-01 14:09:19 +0100

    fix: update the eslint.config file to use standard flat config array format without the deprecated tseslint.config() wrapper.

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
 
 // Import local config (optional, won't error if file doesn't exist)
 let localConfig = { localIgnores: [], localRules: {} };
@@ -13,22 +12,24 @@ try {
   // Local config file doesn't exist, use defaults
 }
 
-export default tseslint.config([
-  globalIgnores(['dist', ...localConfig.localIgnores]),
+export default [
+  { ignores: ['dist', ...localConfig.localIgnores] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...reactRefresh.configs.vite.rules,
       ...localConfig.localRules,
     },
   },
-]);
+];
