e1e6a15e7d749dfdbb26c514dcc64a9e3c91f227
Alex Pegorini <alex_pegorini@hotmail.com>
2025-10-01 01:03:14 +0100

    feature: hide linting tests

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'linting-test']),
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
