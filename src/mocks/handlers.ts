f1e4026735eb864c5a3c40204b079023721cbf7d
Alex Pegorini <alex_pegorini@hotmail.com>
2025-11-04 15:39:20 +0000

    chore: return 200Ok for Mock API example

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -9,9 +9,6 @@ export const handlers: HttpHandler[] = [
   // set up http or graphQL handlers
   http.get<never, never, MockItem[]>('http://localhost:5100/mock', async () => {
     await delay(100);
-    return HttpResponse.json<MockItem[]>([
-      { name: 'Chocolate', imagePath: '/images/chocolate.png' },
-      { name: 'Vanilla', imagePath: '/images/vanilla.png' },
-    ]);
+    return new HttpResponse(null, { status: 200 });
   }),
 ];
